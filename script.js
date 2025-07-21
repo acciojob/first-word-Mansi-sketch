6c67ef977b6d815802d8cc5cba97ecbdb7cad600
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-21 21:48:53 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+	let str = s.split(' ');
+	return str[0];
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
